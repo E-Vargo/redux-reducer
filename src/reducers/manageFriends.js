b698b6e58c156e3930036bbdd0a5353b2942f156
E-Vargo <vargoelliott@gmail.com>
2022-09-29 23:31:10 -0700

    commit tree cb4d37cbc4f10e9908c78478a6cd7dd0548cf460

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return (
+                {...state,
+                    friends: [
+                      ...state.friends,
+                      action.friend
+                    ]
+                }
+            )
+        case "REMOVE_FRIEND":
+            const rFriend = state.friends.find(f => f.id === action.id)
+            const rIndex = state.friends.indexOf(rFriend)
+            return (
+                {...state,
+                friends: [
+                    ...state.friends.slice(0, rIndex),
+                    ...state.friends.slice(rIndex + 1)
+                ]
+                }
+            )
+        default:
+            return state;
+    }
 }
